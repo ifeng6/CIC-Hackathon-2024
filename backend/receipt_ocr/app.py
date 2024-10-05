df6e7a56e39e023ff4da69f620c3b9e8354572f7
Ryan <111664482+riyne@users.noreply.github.com>
2024-10-05 12:43:30 -0700

    removed cleaning data

--- backend/receipt_ocr/app.py
+++ backend/receipt_ocr/app.py
@@ -18,24 +18,23 @@ async def ocr_receipt(file: UploadFile):
         image_data = await file.read()
         image_data_array = np.frombuffer(image_data, np.uint8)
         recognized_text = perform_ocr(image_data_array)
-        cleaned_text = cleanText(recognized_text)
-        return JSONResponse(content=cleaned_text, status_code=200)
+        return JSONResponse(content={'result': recognized_text}, status_code=200)
     else:
         return {"error": "the uploaded file is not an image"}
     
-def cleanText(text):
-    text = text.replace("\f", "")
-    lines = text.split("\n")
-    keywords_to_exclude = [
-        "WHOLESALE", "GREEN TOWN", "Member", "SUBTOTAL", "TAX", "TOTAL", "CHIP Read",
-        "AID", "Seq", "APP#", "VISA", "Resp", "Tran IDs", "Merchant 'ID", "APPROVED",
-        "AMOUNT", "CHANGE", "S TAX", "E TAX", "A TAX", "TOTAL TAX", "TOTAL NUMBER OF ITEMS SOLD",
-        "Thank You", "Please Come Again", "Whse", "Trm", "Trn", "OP", "Items Sold"
-    ]
+# def cleanText(text):
+#     text = text.replace("\f", "")
+#     lines = text.split("\n")
+#     keywords_to_exclude = [
+#         "WHOLESALE", "GREEN TOWN", "Member", "SUBTOTAL", "TAX", "TOTAL", "CHIP Read",
+#         "AID", "Seq", "APP#", "VISA", "Resp", "Tran IDs", "Merchant 'ID", "APPROVED",
+#         "AMOUNT", "CHANGE", "S TAX", "E TAX", "A TAX", "TOTAL TAX", "TOTAL NUMBER OF ITEMS SOLD",
+#         "Thank You", "Please Come Again", "Whse", "Trm", "Trn", "OP", "Items Sold"
+#     ]
     
-    cleaned_data = [
-        line for line in lines if not any(keyword in line for keyword in keywords_to_exclude)
-    ]
+#     cleaned_data = [
+#         line for line in lines if not any(keyword in line for keyword in keywords_to_exclude)
+#     ]
         
-    return cleaned_data
+#     return cleaned_data
 
