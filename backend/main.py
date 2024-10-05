117afc8dee41a499a5cdd683cebbc533a072cf2a
kelvinwong0519@gmail.com <kelvinwong0519@gmail.com>
2024-10-05 10:20:34 -0700

    add boto3

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,20 @@
 from flask import Flask, request
+import boto3
+from dotenv import load_dotenv
+import os
 
-app = Flask(__name__)
+load_dotenv()
 
+app = Flask(__name__, port=8000)
 
-@app.route("/", methods=["GET", "POST"])
+bedrock_runtime = boto3.client(
+    service_name="bedrock-runtime",
+    region_name=os.getenv("AWS_DEFAULT_REGION"),
+    aws_access_key=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    aws_session_token=os.getenv("AWS_SESSION_TOKEN"),
+)
+
+@app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
-
