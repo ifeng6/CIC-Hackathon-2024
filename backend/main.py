3b4146690ce545377612c9e90ea3581b444a65a2
Ivan Hu <ivanhu@dhcp-128-189-227-109.ubcsecure.wireless.ubc.ca>
2024-10-05 13:25:19 -0700

    prompt engineering changes

--- backend/main.py
+++ backend/main.py
@@ -31,20 +31,24 @@ textract_runtime = boto3.client(
 def hello_world():
     return "<p>Hello, World!</p>"
 
-
+# Receives image file and process with ocr
+# Extract food items and their respective information
+# Generate images of the food and recipes
 @app.route("/upload", methods=["POST"])
 def upload():
     if request.method == "POST":
         input = request.json.get("input")
 
         system_prompt = """
-            You are given the output of textual extraction of information of a receipt from a grocery store.
-            For all the items in the receipt that are consumable by humans, such as bread or drinks, output in JSON format where the key is the name of the food item
-            and the value is the quantity and cost of each food item. ONLY INCLUDE FOODS ITEMS THAT CAN BE EATEN OR DRANK. ONLY OUTPUT THE JSON YOU GENERATED. NO OTHER TEXT.
+            You are given an ocr output from a grocery store receipt that includes food items and non-food items. 
+            Filter the items given to include food items and only food items. A food item is defined as something that can be eaten or drank by humans. For example bread, drinks, and desserts can be considered as items to include.
+            Do not include words that aren't English words.
+            For each food item, output a valid JSON object where the key is the name of the item and the value is an object containing the quantity and cost.
+            Do not include non-food items. Output only the JSON and make sure the JSON is valid syntax. No additional text.
         """
 
-        receipt = "=WHOLESALE\nGREEN TOWN $ 5208\n1212, Pine Wood Plaza Or\nGreen Town, CA 34343\n87 Member 585635184442\nE 3081064 BANANAS 1.67 E\nA 7073705 Bluetooth Care 122.34 A\n8143739 Dinning Table 422.99 §\n8523605 Wine Bottle 9.99 E\n8831466 Beer Case 19.90 E\nSUBTOTAL 576.89\nTAX 102.67\n44% TOTAL\nXXXXXXXXXXXX9999 CHIP Read\nAID: — L8G716KSFKGB\nSeq 463882 — APP#: = MAST\nVISA Resp: _ APPROVED\nTran IDs: 2577049117\nMerchant 'ID: 052587\nAPPROVED - Purchase\nAMOUNT : 679.56\n08/19/2021 17:58:17 5208 208 256 208\nOO VTS 67956”\nCHANGE 0.00\nS TAX 9.75% 89.99\nE TAX 7.75% 8.78\nA TAX 2.75% 3.90\nTOTAL TAX 102.67\nTOTAL NUMBER OF ITEMS SOLD = 5\n17:58:17 5208 208 256 208\n2053201443071 1570000\nOP: 208 NAME: SCO LANE #208\nThank You.\nPlease Come Again\nWhse: 5208 Trm: 208 Trn: 256 OP: 208\nItems Sold : 5\nB7 08/19/2021 17:58:17\n\f"
-            
+        receipt = '["OS","ity # 12783","W 4th Ave, Vancouver, BC V6K 1N9","Apples @ 21bs 4.72 E","Bluetooth Speaker 179.99","Cookies 9.99","Popcorn 8.99","Ketchup 5.5","Laundry detergent 14.99","Peanut Butter 6.79","Salmon @ 71bs 84.99","Garlic 84.99","Olive 0i1 13.99","Broccoli @ 21bs 10.90","Amazon T-Shirt 59.71","Grapes @ 91bs 9","Milk @ 4L 8.99","eat 307551 APP: = jz8R","ran ID#: 7979661264","erchant ID: 768273","OVS SSC«STBL AT","yausyeaies 12:33:32 1206 206 256 206","U8Z3ZU881549831 /UUU0"]'
+
         prompt = f"""
             <|begin_of_text|>
             <|start_header_id|>system<|end_header_id|>
@@ -61,7 +65,7 @@ def upload():
             "contentType": "application/json",
             "accept": "application/json",
             "body": json.dumps(
-                {"prompt": prompt, "max_gen_len": 2048, "temperature": 0.2, "top_p": 0.85}
+                {"prompt": prompt, "max_gen_len": 2048, "temperature": 0.25, "top_p": 0.25}
             ),
         }
 
