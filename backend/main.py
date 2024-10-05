602da8c9340d794cdae59e94e00cc0ecb1c645f3
kelvinwong0519@gmail.com <kelvinwong0519@gmail.com>
2024-10-05 13:28:06 -0700

    image gen working

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ import json
 from dotenv import load_dotenv
 from flask_cors import CORS
 import os
+import base64
 
 load_dotenv()
 
@@ -26,6 +27,16 @@ textract_runtime = boto3.client(
     aws_session_token=os.getenv("AWS_SESSION_TOKEN"),
 )
 
+s3 = boto3.client(
+    service_name="s3",
+    region_name=os.getenv("AWS_DEFAULT_REGION"),
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    aws_session_token=os.getenv("AWS_SESSION_TOKEN"),
+)
+
+bucket_name = "cic-hackathon-24-ai-images"
+
 
 @app.route("/")
 def hello_world():
@@ -45,7 +56,7 @@ def upload():
         """
 
         receipt = "=WHOLESALE\nGREEN TOWN $ 5208\n1212, Pine Wood Plaza Or\nGreen Town, CA 34343\n87 Member 585635184442\nE 3081064 BANANAS 1.67 E\nA 7073705 Bluetooth Care 122.34 A\n8143739 Dinning Table 422.99 §\n8523605 Wine Bottle 9.99 E\n8831466 Beer Case 19.90 E\nSUBTOTAL 576.89\nTAX 102.67\n44% TOTAL\nXXXXXXXXXXXX9999 CHIP Read\nAID: — L8G716KSFKGB\nSeq 463882 — APP#: = MAST\nVISA Resp: _ APPROVED\nTran IDs: 2577049117\nMerchant 'ID: 052587\nAPPROVED - Purchase\nAMOUNT : 679.56\n08/19/2021 17:58:17 5208 208 256 208\nOO VTS 67956”\nCHANGE 0.00\nS TAX 9.75% 89.99\nE TAX 7.75% 8.78\nA TAX 2.75% 3.90\nTOTAL TAX 102.67\nTOTAL NUMBER OF ITEMS SOLD = 5\n17:58:17 5208 208 256 208\n2053201443071 1570000\nOP: 208 NAME: SCO LANE #208\nThank You.\nPlease Come Again\nWhse: 5208 Trm: 208 Trn: 256 OP: 208\nItems Sold : 5\nB7 08/19/2021 17:58:17\n\f"
-            
+
         prompt = f"""
             <|begin_of_text|>
             <|start_header_id|>system<|end_header_id|>
@@ -62,7 +73,12 @@ def upload():
             "contentType": "application/json",
             "accept": "application/json",
             "body": json.dumps(
-                {"prompt": prompt, "max_gen_len": 2048, "temperature": 0.5, "top_p": 0.9}
+                {
+                    "prompt": prompt,
+                    "max_gen_len": 2048,
+                    "temperature": 0.5,
+                    "top_p": 0.9,
+                }
             ),
         }
 
@@ -72,6 +88,45 @@ def upload():
         print(generated_text)
         return "<p>Upload complete</p>"
 
+
+def generate_image(food_str: str):
+    food_str = food_str.lower().replace(" ", "")
+    response = s3.list_objects_v2(Bucket=bucket_name)
+    objects = response.get("Contents", [])
+    food_str_exists = any(obj["Key"] == f"{food_str}.png" for obj in objects)
+
+    if food_str_exists:
+        url = s3.generate_presigned_url(
+            "get_object",
+            Params={"Bucket": bucket_name, "Key": f"{food_str}.png"},
+            ExpiresIn=3600,
+        )
+        return url
+
+    # otherwise generate the image
+    kwargs = {
+        "modelId": "amazon.titan-image-generator-v2:0",
+        "contentType": "application/json",
+        "accept": "application/json",
+        "body": f'{{"textToImageParams":{{"text":"{food_str}"}},"taskType":"TEXT_IMAGE","imageGenerationConfig":{{"cfgScale":8,"seed":0,"quality":"standard","width":1024,"height":1024,"numberOfImages":1}}}}',
+    }
+    response = bedrock_runtime.invoke_model(**kwargs)
+    body = json.loads(response["body"].read())
+    base64_image_data = body["images"][0]
+
+    image_data = base64.b64decode(base64_image_data)
+
+    object_key = f"{food_str}.png"
+    s3.put_object(Body=image_data, Bucket=bucket_name, Key=object_key)
+    url = s3.generate_presigned_url(
+        "get_object",
+        Params={"Bucket": bucket_name, "Key": object_key},
+        ExpiresIn=3600,
+    )
+    return url
+
+
+print(generate_image("raw chicken"))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
